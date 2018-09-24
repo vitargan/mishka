53b7dc5c388cad29717f689b9cad760fb937723e
vitargan <36110701+vitargan@users.noreply.github.com>
2018-09-24 22:59:21 +0300

    разметка

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var gulp = require("gulp");
-var sass = require("gulp-less");
+var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
